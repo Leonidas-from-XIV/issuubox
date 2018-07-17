2a0ad6fd534163d4bf5551f642c7a0041d4510a8
Marek Kubica <marek@xivilization.net>
2018-07-17 20:35:51 +0200

    Use Cmdliner to read command line arguments

--- src/issuubox.ml
+++ src/issuubox.ml
@@ -1,4 +1,12 @@
 open Base
 
+let main api_key folder =
+  Caml.print_endline @@ "API " ^ api_key;
+  Caml.print_endline @@ "FOLDER " ^ folder
+
 let () =
-  List.iter ~f:Caml.print_endline ["Hello"]
+  let open Cmdliner in
+  let api_key = Arg.(required & pos 0 (some string) None & info ~docv:"API-KEY" []) in
+  let folder = Arg.(required & pos 1 (some dir) None & info ~docv:"FOLDER" []) in
+  let main_t = Term.(const main $ api_key $ folder) in
+  Term.(exit @@ eval (main_t, info "issuubox"))
